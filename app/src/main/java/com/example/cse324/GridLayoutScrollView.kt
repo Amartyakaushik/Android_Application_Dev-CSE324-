543f2e642f4bf9289bb9c443a33b0c7394abca0d
Amartya <kamartya58@gmail.com>
2024-11-29 20:01:02 +0530

    GridLayoutScrollView and horizontal scroll view

---
+++ app/src/main/java/com/example/cse324/GridLayoutScrollView.kt
@@ -0,0 +1,11 @@
+package com.example.cse324
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GridLayoutScrollView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_grid_layout_scroll_view)
+    }
+}
