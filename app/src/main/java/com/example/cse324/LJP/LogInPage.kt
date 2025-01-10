af0ae4cb50d49e5300d9566d7b9c821ff89dc471
Amartya <kamartya58@gmail.com>
2025-01-10 21:37:35 +0530

    Sample login page for the ljp project implementing the login feature along with the authentication

---
+++ app/src/main/java/com/example/cse324/LJP/LogInPage.kt
@@ -0,0 +1,12 @@
+package com.example.cse324.LJP
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cse324.R
+
+class LogInPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in_page)
+    }
+}
