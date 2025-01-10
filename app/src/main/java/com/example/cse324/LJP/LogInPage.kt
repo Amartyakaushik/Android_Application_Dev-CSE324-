3b60bb527027a16c792c40e871656e72dbb93b69
Amartya <kamartya58@gmail.com>
2025-01-10 21:42:44 +0530

    Sample login Page implementation for the authentication purpose.

--- app/src/main/java/com/example/cse324/LJP/LogInPage.kt
+++ app/src/main/java/com/example/cse324/LJP/LogInPage.kt
@@ -1,12 +1,25 @@
 package com.example.cse324.LJP
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.cse324.R
+import com.example.cse324.databinding.ActivityLogInPageBinding
 
 class LogInPage : AppCompatActivity() {
+    private lateinit var binding : ActivityLogInPageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_log_in_page)
+        binding = ActivityLogInPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var email = binding.email
+        var password = binding.password
+        var loginBtn = binding.loginBtn
+        loginBtn.setOnClickListener{
+            startActivity(Intent(this,SplashScreen::class.java))
+
+        }
     }
 }
